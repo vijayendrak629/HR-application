df79771c463e03949687ba14702845d796ad8850
DESKTOP-8I52BO0\vijay <vijayendra.kumbhar@martechs.io>
2023-10-27 21:35:06 +0530

    file structure changed

---
+++ Database/upcomingdb.php
@@ -0,0 +1,33 @@
+<?php
+include "Database/connection.php";
+
+$tomorrow = date('Y-m-d', strtotime('+1 day'));
+$birthdayQuery = "SELECT full_name, dob FROM employee_details WHERE DATE_FORMAT(dob, '%m-%d') = DATE_FORMAT('$tomorrow', '%m-%d')";
+$birthdayResult = $connection->query($birthdayQuery);
+
+$upcomingBirthdays = [];
+
+if ($birthdayResult->num_rows > 0) {
+    while ($row = $birthdayResult->fetch_assoc()) {
+        $upcomingBirthdays[] = $row;
+    }
+}
+
+// Query for upcoming work anniversaries
+$anniversaryQuery = "SELECT full_name, joining_date FROM employee_details WHERE DATE_FORMAT(joining_date, '%m-%d') = DATE_FORMAT('$tomorrow', '%m-%d')";
+$anniversaryResult = $connection->query($anniversaryQuery);
+
+$upcomingAnniversaries = [];
+
+if ($anniversaryResult->num_rows > 0) {
+    while ($row = $anniversaryResult->fetch_assoc()) {
+        $joiningDate = new DateTime($row['joining_date']);
+        $currentDate = new DateTime();
+
+        // Calculate the work anniversary year
+        $workAnniversaryYear = $currentDate->diff($joiningDate)->y + 1;
+
+        $row['work_anniversary_year'] = $workAnniversaryYear;
+        $upcomingAnniversaries[] = $row;
+    }
+}
--- login_process.php
+++ login_process.php
@@ -1,24 +1,13 @@
 <?php
-// Establish a database connection (update the credentials accordingly).
-$dbHost = 'localhost';
-$dbUser = 'root';
-$dbPass = '';
-$dbName = 'internal';
 
-$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
-
-// Check the connection.
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+include "Database/connection.php";
 // Get username and password from the form.
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 // Query the database to check for user credentials.
 $sql = "SELECT * FROM data WHERE username = '$username' AND password = '$password'";
-$result = $conn->query($sql);
+$result = $connection->query($sql);
 
 if ($result->num_rows == 1) {
     // Login successful.
@@ -31,5 +20,3 @@ if ($result->num_rows == 1) {
     
 }
 
-$conn->close();
-?>
